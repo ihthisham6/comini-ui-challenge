3c5d4faa6983eac33d12d7b65c4b2da2643cd6af
Ayesha Insha Arsh <ayeshainshaarsh@Ayeshas-Air.bbrouter>
2025-05-22 05:42:06 +0530

    Clean state for deployment

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},  // Use this for Tailwind with PostCSS
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Resolves '@' to 'src'
+      '@': resolve(__dirname, 'src')
     },
   },
   server: {
